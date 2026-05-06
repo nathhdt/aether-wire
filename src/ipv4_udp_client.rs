d8b5bbd24b1df4faf2bf8e415fb6716dfe72ead1
nathhdt <nathhdt@pm.me>
2026-05-06 19:26:40 +0200

    fix: more information on UDP commands (not implemented yet)

--- src/ipv4_udp_client.rs
+++ src/ipv4_udp_client.rs
@@ -6,7 +6,10 @@ use crate::cli::Ipv4UdpClientArgs;
 
 /// runs the UDP client, connects to a server, and benchmarks the wire
 pub fn run(args: Ipv4UdpClientArgs) -> Result<()> {
-    println!("UDP client {}", args.server);
+    println!(
+        "UDP client to server {}:{} (not implemented yet)",
+        args.server, args.port
+    );
 
     Ok(())
 }
--- src/ipv4_udp_server.rs
+++ src/ipv4_udp_server.rs
@@ -6,7 +6,10 @@ use crate::cli::Ipv4UdpServerArgs;
 
 /// runs the UDP server, listens for a connection, and benchmarks the wire
 pub fn run(args: Ipv4UdpServerArgs) -> Result<()> {
-    println!("UDP server {}", args.bind);
+    println!(
+        "UDP server {}:{} (not implemented yet)",
+        args.bind, args.port
+    );
 
     Ok(())
 }
