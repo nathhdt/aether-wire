b32284644e13cc867349f04716de4b303c818648
nathhdt <nathhdt@pm.me>
2026-06-22 23:49:51 +0200

    feat(aw): add interface is up & have an IP check for 'aw udp/server' command

--- src/udp/mod.rs
+++ src/udp/mod.rs
@@ -4,12 +4,27 @@ use anyhow::Result;
 
 use crate::cli::commands::udp::UdpConfig;
 use crate::utils::format::human_bps;
-use crate::utils::network::interfaces::interface_exists;
+use crate::utils::network::constants::IF_OPER_UP;
+use crate::utils::network::interfaces::{
+    get_interface, get_interface_addresses, get_interface_details,
+};
 
 pub fn run(config: UdpConfig) -> Result<()> {
     // check if interface exists
-    if !config.iface.is_empty() && !interface_exists(&config.iface) {
-        anyhow::bail!("interface '{}' not found", config.iface);
+    let iface = get_interface(&config.iface)?;
+
+    let details = get_interface_details(iface.index)?
+        .ok_or_else(|| anyhow::anyhow!("no Netlink data for interface '{}'", config.iface))?;
+
+    // interface must be up
+    if details.operstate != Some(IF_OPER_UP) {
+        anyhow::bail!("interface '{}' is not up", config.iface);
+    }
+
+    // interface must have at least one address
+    let addresses = get_interface_addresses(iface.index)?;
+    if addresses.is_empty() {
+        anyhow::bail!("interface '{}' has no configured addresses", config.iface);
     }
 
     println!(
