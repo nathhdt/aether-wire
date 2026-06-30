78b02e261bbb31eed8c114821952d7cf9876b89a
nathhdt <nathhdt@pm.me>
2026-06-30 19:28:55 +0200

    feat(utils-system): add time module (CLOCK_MONOTONIC getter)

--- src/utils/system/mod.rs
+++ src/utils/system/mod.rs
@@ -2,3 +2,4 @@
 
 pub mod host;
 pub mod sysctl;
+pub mod time;
---
+++ src/utils/system/time.rs
@@ -0,0 +1,10 @@
+//! monotonic time module
+
+use rustix::time::{ClockId, clock_gettime};
+
+/// returns current CLOCK_MONOTONIC
+pub fn monotonic_now_ns() -> u64 {
+    let ts = clock_gettime(ClockId::Monotonic);
+
+    (ts.tv_sec as u64) * 1_000_000_000 + ts.tv_nsec as u64
+}
